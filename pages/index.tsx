c6e7a349f94c98061f36f12da4d889439b3d648d
Alexander Dang <alexdang1993374@gmail.com>
2023-07-31 08:15:14 -0700

    feat: begin project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+  <div>
+    <p className="text-6xl text-green-500">Hello Einstein</p>
+
+    <Button variant='outline'>Click me</Button>
+  </div>
+  );
+}
