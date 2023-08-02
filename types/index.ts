86431d91e44381b8489bc1370373edf560616ac3
Alexander Dang <alexdang1993374@gmail.com>
2023-08-01 21:59:43 -0700

    feat: finish dashboard

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,11 @@ export interface IRoutes {
   href: string;
   color?: string;
 }
+
+export interface ITools {
+  label: string;
+  icon: LucideIcon;
+  color: string;
+  bgColor: string;
+  href: string;
+}
