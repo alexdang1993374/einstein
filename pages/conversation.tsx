14b7efba10c453175413c94a71fa80c9f2c6e6d4
Alexander Dang <alexdang1993374@gmail.com>
2023-08-08 16:59:34 -0700

    fix: preline

--- pages/conversation.tsx
+++ pages/conversation.tsx
@@ -128,7 +128,9 @@ const ConversationPage = () => {
                 >
                   {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
 
-                  <p className="text-sm">{message.content}</p>
+                  <p className="text-sm whitespace-pre-line">
+                    {message.content}
+                  </p>
                 </div>
               ))}
             </div>
