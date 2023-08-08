d156f67ec3b3421b98c43793da602fc82c2e7e73
Alexander Dang <alexdang1993374@gmail.com>
2023-08-08 16:28:44 -0700

    feat: conversation working

---
+++ pages/api/conversation.ts
@@ -0,0 +1,47 @@
+import { getAuth } from "@clerk/nextjs/server";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  if (req.method !== "POST") {
+    res.json("Should be POST request");
+    return;
+  }
+
+  try {
+    const { userId } = getAuth(req);
+    const { messages } = await req.body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!configuration.apiKey) {
+      return new NextResponse("OpenAI API Key not configured", { status: 500 });
+    }
+
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages,
+    });
+
+    return res.json(response.data.choices[0].message?.content);
+  } catch (error) {
+    console.log("[CONVERSATION_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
--- pages/conversation.tsx
+++ pages/conversation.tsx
@@ -1,5 +1,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 import { MessageSquare } from "lucide-react";
+import { ChatCompletionRequestMessage } from "openai";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
@@ -11,6 +14,8 @@ import { Input } from "@/components/ui/input";
 import { formSchema } from "@/constants/schema";
 
 const ConversationPage = () => {
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -21,7 +26,28 @@ const ConversationPage = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      const userMessage: ChatCompletionRequestMessage = {
+        role: "user",
+        content: values.prompt,
+      };
+      const newMessages = [...messages, userMessage];
+
+      const response = await axios.post("/api/conversation", {
+        messages: newMessages,
+      });
+
+      setMessages((prev) => [
+        ...prev,
+        userMessage,
+        { role: "user", content: response.data },
+      ]);
+
+      form.reset();
+    } catch (error) {
+      // TODO: Open Pro Modal
+      console.log(error);
+    }
   };
 
   return (
@@ -45,7 +71,10 @@ const ConversationPage = () => {
                 <FormField
                   name="prompt"
                   render={({ field }) => (
-                    <FormItem className="col-span-12 lg:col-span-10">
+                    <FormItem
+                      key={field.name}
+                      className="col-span-12 lg:col-span-10"
+                    >
                       <FormControl className="m-0 p-0">
                         <Input
                           className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
@@ -66,6 +95,16 @@ const ConversationPage = () => {
                 </Button>
               </form>
             </Form>
+          </div>
+
+          <div className="space-y-4 mt-4">
+            <div className="flex flex-col-reverse gap-y-4">
+              {messages.map((message, index) => (
+                <div key={index + (message.content || "")}>
+                  {message.content}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
