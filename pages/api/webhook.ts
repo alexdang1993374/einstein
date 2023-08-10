ed82fd18a0a0259d04a21a30e75033446d28b27c
Alexander Dang <alexdang1993374@gmail.com>
2023-08-10 16:25:34 -0700

    feat: stripe webhook

---
+++ pages/api/webhook.ts
@@ -0,0 +1,75 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { headers } from "next/headers";
+import Stripe from "stripe";
+
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  if (req.method !== "POST") {
+    res.json("Should be POST request");
+    return;
+  }
+
+  const body = await req.body;
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    return res.status(400).json({ message: `Webhook Error: ${error.message}` });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+
+    if (!session?.metadata?.userId) {
+      return res.status(400).json({ message: "User ID is required" });
+    }
+
+    await prismadb.userSubscription.create({
+      data: {
+        userId: session?.metadata?.userId,
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer as string,
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
+
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+
+    await prismadb.userSubscription.update({
+      where: {
+        stripeSubscriptionId: subscription.id,
+      },
+      data: {
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
+
+  return res.status(200).json({ message: "Webhook Success" });
+}
